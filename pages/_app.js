1d4931844110a306daa75dafd1bc8fb9b945d59b
Domitrius Clark <domitriusaclark@gmail.com>
2022-01-29 13:42:47 -0500

    Installed Chakra and it's matching dependencies from emotion and framer. Configured it in our _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
