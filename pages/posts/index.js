6d5e12162ce7b8ba25b3edd770e9d56a274bc771
Domitrius Clark <domitriusaclark@gmail.com>
2022-02-02 04:03:01 -0500

    Bringing the code in line with the workshop by making some Nav changes / page changes.

---
+++ pages/posts/index.js
@@ -0,0 +1,5 @@
+import { Heading } from "@chakra-ui/react"
+
+export default function PostsPage() {
+  return <Heading>Posts</Heading>
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <Flex bg="gray.600" color="white" w="full" sx={{ gap: "12px" }} align="center" pl={3} py={4}>
       <Link href="/">Home</Link>
+      <Link href="/posts">Posts</Link>
       <Link href="/projects">Projects</Link>
-      <Link href="/contact">Contact</Link>
     </Flex>
   )
 };
