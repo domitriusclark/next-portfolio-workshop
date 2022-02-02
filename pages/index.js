d2b6f3b89c81871784d07c45b0a7fd0e1d4efda7
Domitrius Clark <domitriusaclark@gmail.com>
2022-02-02 01:28:40 -0500

    Added mailto link to our bio

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <Heading as="h1">Domitrius Clark</Heading>
         <Heading size="md" as="h3">DX Engineer & Community creator</Heading>
       </VStack>
-      <Text textAlign="center">👋 heyo! My names Domitrius. Ive been a UI/DX Engineer over the past 5 years. Finding solutions while improving someones ability to vist or build websites is my passion. I love video games, anime, and spending my time with those I love. <br /> Lets connect 👇</Text>
+      <Text textAlign="center">👋 heyo! My names Domitrius. Ive been a UI/DX Engineer over the past 5 years. Finding solutions while improving someones ability to vist or build websites is my passion. I love video games, anime, and spending my time with those I love. <br /> ✉️ <Link href="mailto:youremail@gmail.com">Lets connect</Link> ✉️ </Text>
       <HStack spacing="20px" justifySelf="center">
         <Link href="https://twitter.com/domitriusclark" target="_blank">
           <Icon color="blue.200" as={FaTwitter} />
