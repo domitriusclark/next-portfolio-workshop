10c4bf51ccd8c55535f8dae748e212c9b9d4ae39
Domitrius Clark <domitriusaclark@gmail.com>
2022-02-01 19:56:38 -0500

    Finished styles on our home page.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  Heading,
+  Flex,
+  VStack,
+  HStack,
+  Icon,
+  Text,
+  Link,
+  Avatar
+} from '@chakra-ui/react';
+
+import { FaTwitter, FaGithub, } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <Heading as="h1">Home</Heading>
+    <Flex direction="column" align="center" sx={{ gap: "32px" }} justify="center">
+      <Avatar src="/anime-dom.png" size="2xl" />
+      <VStack>
+        <Heading as="h1">Domitrius Clark</Heading>
+        <Heading size="md" as="h3">DX Engineer & Community creator</Heading>
+      </VStack>
+      <Text textAlign="center">👋 heyo! My name's Domitrius. I've been a UI/DX Engineer over the past 5 years. Finding solutions while improving someones ability to vist or build websites is my passion. I love video games, anime, and spending my time with those I love. <br /> Let's connect 👇</Text>
+      <HStack spacing="20px" justifySelf="center">
+        <Link href="https://twitter.com/domitriusclark" target="_blank">
+          <Icon color="blue.200" as={FaTwitter} />
+        </Link>
+        <Link href="https://github.com/domitriusclark" target='_blank'>
+          <Icon color="black" as={FaGithub} />
+        </Link>
+      </HStack>
+    </Flex>
   )
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,9 @@ import Navbar from '@components/Navbar';
 
 export default function Layout({ children }) {
   return (
-    <Flex w="full" h="full" direction="column">
+    <Flex w="full" height="100vh" direction="column">
       <Navbar />
-      <Container display="flex" justifyContent="center" maxW="2xl">
+      <Container display="flex" h="100%" justifyContent="center" maxW="2xl">
         {children}
       </Container>
     </Flex>
