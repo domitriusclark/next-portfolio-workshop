f84c14a78535e758e6bd474dbcc9079c1a098a14
Domitrius Clark <domitriusaclark@gmail.com>
2022-02-01 20:21:08 -0500

    Added a new ProjectCard component to feed new projects to as we create them in the future. We took that card and rendered our portfolio project to start the page off.

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,14 @@
-import { Heading } from '@chakra-ui/react';
+import { VStack } from "@chakra-ui/react";
+import ProjectCard from '@components/ProjectCard';
 
 export default function Projects() {
   return (
-    <Heading as="h1">Projects page</Heading>
+    <VStack spacing={10} mt={10} justify="flex-start" direction="column">
+      <ProjectCard
+        image="/portfolio.png"
+        name="Portfolio"
+        link="https://github.com/domitriusclark/portfolio"
+      />
+    </VStack>
   )
 }
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,24 @@
+import {
+  VStack,
+  Heading,
+  Text,
+  Link,
+  Icon,
+  Image,
+  HStack,
+} from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+
+export default function ProjectCard({ image, name, link }) {
+  return (
+    <VStack h="200px" w="300px" >
+      <Image borderRadius="8px" boxShadow="lg" width="full" src={image} alt="A screenshot of the landing page of my portfolio project" />
+      <HStack w="100%" justify="space-between" pt={3}>
+        <Text>{name}</Text>
+        <Link href={link} target="_blank">
+          <Icon as={FaGithub} color="black" />
+        </Link>
+      </HStack>
+    </VStack>
+  )
+}
